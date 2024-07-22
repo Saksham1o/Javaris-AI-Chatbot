2306050dc6d96fcea7a7e801107b8bfec2dacd6e
Saksham1o <sakshampatiyar26@gmail.com>
2024-07-22 18:42:59 +0530

    made bot

---
+++ bot_scrapper.py
@@ -0,0 +1,8 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webcriver.common.by import By
+import warnings
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
+
